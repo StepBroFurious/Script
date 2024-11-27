c9488f984fdc53cdd75ae02d9ca1d21a522a5cb4
Furious <87245695+StepBroFurious@users.noreply.github.com>
2024-11-27 11:58:00 -0800

    Update Octohook.lua

--- Octohook.lua
+++ Octohook.lua
@@ -67,7 +67,7 @@ local library = {
         ['fps'] = 0;
         ['ping'] = 0;
     };
-    Font = Drawing.Fonts.Monospace;
+    Font = Drawing.Fonts.UI;
     images = {
         ['gradientp90'] = 'https://raw.githubusercontent.com/portallol/luna/main/modules/gradient90.png';
         ['gradientp45'] = 'https://raw.githubusercontent.com/portallol/luna/main/modules/gradient45.png';
